5f0596a9acc04732a8a4244a16b534d1a18290e1
Sebastian Davids <sdavids@gmx.de>
2018-09-18 13:25:23 +0200

    Cleanup release.sh
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- release.sh
+++ release.sh
@@ -2,4 +2,8 @@
 echo -n "PGP Private Key Password: "
 read -s gpgpassword
 echo
-./gradlew -Prelease -Psigning.password="${gpgpassword}" clean build generatePomFileForMavenJavaPublication bintrayUpload
+./gradlew -Prelease -Psigning.password="${gpgpassword}" \
+  clean \
+  build \
+  generatePomFileForMavenJavaPublication \
+  bintrayUpload
